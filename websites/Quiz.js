54cde950ee58b8623def276d4e0d9a29c0467c81
Mai Tran <76891395+keasark@users.noreply.github.com>
2023-10-31 19:40:11 +0700

    Add files via upload

---
+++ websites/Quiz.js
@@ -0,0 +1,91 @@
+function Quiz(questions) {
+    this.score = 0;
+    this.questions = questions;
+    this.questionIndex = 0;
+}
+ 
+Quiz.prototype.getQuestionIndex = function() {
+    return this.questions[this.questionIndex];
+}
+ 
+Quiz.prototype.guess = function(answer) {
+    if(this.getQuestionIndex().isCorrectAnswer(answer)) {
+        this.score++;
+    }
+ 
+    this.questionIndex++;
+}
+ 
+Quiz.prototype.isEnded = function() {
+    return this.questionIndex === this.questions.length;
+}
+ 
+ 
+function Question(text, choices, answer) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+}
+ 
+Question.prototype.isCorrectAnswer = function(choice) {
+    return this.answer === choice;
+}
+ 
+ 
+function populate() {
+    if(quiz.isEnded()) {
+        showScores();
+    }
+    else {
+        // show question
+        var element = document.getElementById("question");
+        element.innerHTML = quiz.getQuestionIndex().text;
+ 
+        // show options
+        var choices = quiz.getQuestionIndex().choices;
+        for(var i = 0; i < choices.length; i++) {
+            var element = document.getElementById("choice" + i);
+            element.innerHTML = choices[i];
+            guess("btn" + i, choices[i]);
+        }
+ 
+        showProgress();
+    }
+};
+ 
+function guess(id, guess) {
+    var button = document.getElementById(id);
+    button.onclick = function() {
+        quiz.guess(guess);
+        populate();
+    }
+};
+ 
+ 
+function showProgress() {
+    var currentQuestionNumber = quiz.questionIndex + 1;
+    var element = document.getElementById("progress");
+    element.innerHTML = "Question " + currentQuestionNumber + " of " + quiz.questions.length;
+};
+ 
+function showScores() {
+    var gameOverHTML = "<h1>Result</h1>";
+    gameOverHTML += "<h2 id='score'> Your scores: " + quiz.score + "</h2>";
+    var element = document.getElementById("quiz");
+    element.innerHTML = gameOverHTML;
+};
+ 
+// create questions 
+var questions = [
+    new Question("What are the common symptoms of COVID-19?", ["Continuous Cough", "Fever","Tiredness", "All"], "All"),
+    new Question("Which of the following people is COVID-19 more dangerous for?", ["Children", "Older people", "Ill people", "All"], "All"),
+    new Question("When should fabric face masks be worn?", ["On public transport", "Crowded spaces","In small shops", "All"], "All"),
+    new Question("A person who has no symptoms of COVID-19 is not a risk to others", ["True", "False", "Sometimes", "Not Sure"], "False"),
+    new Question("The novel coronavirus responsible for the outbreak is called ...", ["COVID-19", "SARS-CoV-2", "2019-nCoV", "MERS-CoV-2"], "SARS-CoV-2")
+];
+ 
+// create quiz
+var quiz = new Quiz(questions);
+ 
+// display quiz
+populate();
